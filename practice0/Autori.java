4c6589dbbde22653d9009d3a53223d7aa5828796
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-08-07 16:10:47 +0800

    problem 1 - Autori

--- practice0/Autori.java
+++ practice0/Autori.java
@@ -4,7 +4,6 @@ public class Autori {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         String word = myObj.nextLine();
-
         System.out.print(word.charAt(0));
 
         for (int i = 1; i < word.length(); i++) {
